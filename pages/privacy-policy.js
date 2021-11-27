dd8a819c12f52772971b304bb1ac2288a8747a09
quitequinn <qkeave@gmail.com>
2021-11-26 16:08:01 -0800

    Cleanup

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -1,61 +1,60 @@
-import React, { useEffect, useState } from "react"
-
-
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-   return (
-      <>
-         <div>
-            <h1>Liiift Studio Privacy Policy</h1>
-            <br/>
-            <p>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make
-            a purchase from https://opticalfont.com/ (the “Site”).
-            <br/>
-            <br/>
-
-            PERSONAL INFORMATION WE COLLECT<br/>
-
-            When you visit the Site, we automatically collect certain information about your device, including information
-            about your web browser, IP address, time zone, and some of the cookies that are installed on your device.
-            Additionally, as you browse the Site, we collect information about the individual web pages or products that you
-            view, what websites or search terms referred you to the Site, and information about how you interact with the
-            Site. We refer to this automatically-collected information as “Device Information.”
-            <br/>
-
-            We collect Device Information using the following technologies:
-            <br/>
-
-
-            - “Log files” track actions occurring on the Site, and collect data including your IP address, browser type,
-            Internet service provider, referring/exit pages, and date/time stamps.
-            - “Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the
-            Site.
-
-            <br/>
-            <br/>
-
-            DO NOT TRACK<br/>
-
-            Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal
-            from your browser.
-            <br/>
-            <br/>
-
-            CHANGES<br/>
-
-            We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or
-            for other operational, legal or regulatory reasons.
-            <br/><br/>
-
-            CONTACT US<br/> 
-
-            For more information about our privacy practices, if you have questions, or if you would like to make a complaint,
-            please contact us by e-mail at hello@liiift.studio or by mail using the details provided below:
-            <br/>
-
-            520 E 1st Ave, Vancouver, BC, V5T 1E1, Canada
-            </p>
-         </div>
-      </>
-   )
+  return (
+    <>
+      <div>
+        <h1>Liiift Studio Privacy Policy</h1>
+        <br />
+        <p>
+          This Privacy Policy describes how your personal information is
+          collected, used, and shared when you visit or make a purchase from
+          https://opticalfont.com/ (the “Site”).
+          <br />
+          <br />
+          PERSONAL INFORMATION WE COLLECT
+          <br />
+          When you visit the Site, we automatically collect certain information
+          about your device, including information about your web browser, IP
+          address, time zone, and some of the cookies that are installed on your
+          device. Additionally, as you browse the Site, we collect information
+          about the individual web pages or products that you view, what
+          websites or search terms referred you to the Site, and information
+          about how you interact with the Site. We refer to this
+          automatically-collected information as “Device Information.”
+          <br />
+          We collect Device Information using the following technologies:
+          <br />
+          - “Log files” track actions occurring on the Site, and collect data
+          including your IP address, browser type, Internet service provider,
+          referring/exit pages, and date/time stamps. - “Web beacons,” “tags,”
+          and “pixels” are electronic files used to record information about how
+          you browse the Site.
+          <br />
+          <br />
+          DO NOT TRACK
+          <br />
+          Please note that we do not alter our Site’s data collection and use
+          practices when we see a Do Not Track signal from your browser.
+          <br />
+          <br />
+          CHANGES
+          <br />
+          We may update this privacy policy from time to time in order to
+          reflect, for example, changes to our practices or for other
+          operational, legal or regulatory reasons.
+          <br />
+          <br />
+          CONTACT US
+          <br />
+          For more information about our privacy practices, if you have
+          questions, or if you would like to make a complaint, please contact us
+          by e-mail at hello@liiift.studio or by mail using the details provided
+          below:
+          <br />
+          520 E 1st Ave, Vancouver, BC, V5T 1E1, Canada
+        </p>
+      </div>
+    </>
+  );
 }
