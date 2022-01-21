4cb609f993d74e05b68d10220ea0fdcf7ee23f3e
Liiift <94940586+Liiift@users.noreply.github.com>
2022-01-21 08:30:02 -0800

    Add chrome store links to install buttons

--- components/landing-layout.js
+++ components/landing-layout.js
@@ -25,8 +25,7 @@ export default function LandingLayout({ children, imgSrc }) {
                 <Typography variant='h1'>
                   <nobr>Varied fonts for</nobr> <br /> varied vision.
                 </Typography>
-                {/* https://chrome.google.com/webstore/detail/optical/jgnimjfkbkjejchhmpocakifegpakcad */}
-                <Button variant="string" href=""
+                <Button variant="string" href="https://chrome.google.com/webstore/detail/optical/jgnimjfkbkjejchhmpocakifegpakcad"
                   sx={{
                     my: 2,
                     border: 0,
@@ -72,7 +71,7 @@ export default function LandingLayout({ children, imgSrc }) {
           <Grid container sm={12} align={'center'} spacing={2}maxWidth={'426px'}>
             <Grid item sm={6}>
               <Button
-                href=""
+                href="https://chrome.google.com/webstore/detail/optical/jgnimjfkbkjejchhmpocakifegpakcad"
                 variant="string"
                 sx={{
                   my: 1,
@@ -128,7 +127,7 @@ export default function LandingLayout({ children, imgSrc }) {
           <Grid container sm={12} pb={10} align={'center'} justifyContent={'space-around'} maxWidth={'426px'}>
             <Grid item sm={6}>
               <Button
-                href=""
+                href="https://chrome.google.com/webstore/detail/optical/jgnimjfkbkjejchhmpocakifegpakcad"
                 variant="string"
                 sx={{
                   my: 1,
