fdcdce46cbd5e3bb5eb95c759e33d12ae2a95db4
quitequinn <qkeave@gmail.com>
2022-04-01 12:28:48 -0700

    Removing GH-Pages changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  // https://github.com/vercel/next.js/issues/21079
-  // Remove this workaround whenever the issue is fixed
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
 }
